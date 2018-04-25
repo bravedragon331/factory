ae3c69422cbf68bd18e874f0334d3e8510e603a5
unknown <bravedragon623@yandex.com>
2018-04-25 11:41:43 +0300

    sub-material

--- config/database.js
+++ config/database.js
@@ -5,5 +5,6 @@ module.exports = {
     'user': 'root',
     'password': 'root',
     'database': 'project'
-  },  
+  },
+  'database': 'project'
 };
--- models/submaterial.js
+++ models/submaterial.js
@@ -3,6 +3,7 @@ var db     = require('./db');
 var createSubMaterial = function(body, callback){
   db.query('INSERT INTO submaterial (code, name, type1, type2, status) values (?,?,?,?,?)', [body.code, body.name, body.type1, body.type2, body.status], function(err){
     if(err){
+      console.log(err);
       if (err.code === 'ER_DUP_ENTRY') {
         // If we somehow generated a duplicate user id, try again
         return createSubMaterial(body, callback);
@@ -14,7 +15,9 @@ var createSubMaterial = function(body, callback){
 }
 
 var addSubMaterial = function(body, callback){
+  console.log(body);
   db.query('SELECT * FROM submaterial WHERE code = ?', [body.code], function(err, rows) {
+    console.log(err);
     if (err)
       return callback(err);
     if (rows.length) {
