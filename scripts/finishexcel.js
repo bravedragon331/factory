c2bb1b46abf099cc9e8c0776e24defaf40ab5451
bravedragon623 <bravedragon623@yandex.com>
2018-05-25 16:36:13 +0300

    upload finish

--- scripts/finishexcel.js
+++ scripts/finishexcel.js
@@ -34,11 +34,11 @@ var finishExcel = function(path, orderid, callback){
     var exceldata = xlsx.parse(path)[0].data;    
     var fails = [];
     const preprocess = function(data){
-      console.log(orderid);
+      console.log(data);
       var tmp = [], order_n = -1, po_n = -1, size_n = 01;      
       for(var i = 0; i < orderdetails.length; i++){        
-          if((orderdetails[i].orderid == orderid) && (orderdetails[i].po.toLowerCase() == data[1].toLowerCase())
-            && (orderdetails[i].colorname.toLowerCase() == data[3].toLowerCase()) && (orderdetails[i].style.toLowerCase() == data[0].toLowerCase()))
+          if((orderdetails[i].orderid == orderid) && (orderdetails[i].po.toLowerCase() == data[1].toString().toLowerCase())
+            && (orderdetails[i].colorname.toLowerCase() == data[2].toLowerCase()) && (orderdetails[i].style.toLowerCase() == data[0].toLowerCase()))
           {
             po_n = i;
             tmp.push(orderid);
@@ -52,7 +52,7 @@ var finishExcel = function(path, orderid, callback){
         tmp = [];
         return tmp;
       }
-      tmp.push(data[2]);
+      tmp.push(data[3]);
       tmp.push(data[4]);
       tmp.push(data[5]);
       tmp.push(data[6]);
@@ -62,7 +62,6 @@ var finishExcel = function(path, orderid, callback){
       tmp.push(data[10]);
       tmp.push(data[11]);
       tmp.push(data[12]);
-      tmp.push(data[13]);
       return tmp;
     }
     const add = function(index){
@@ -74,11 +73,10 @@ var finishExcel = function(path, orderid, callback){
         }else {
           callback(null, fails);
         }
-      }else{
-        var myDate = new Date((Number(tmp[3]) - (25567 + 1))*86400*1000);
+      }else{        
         var data = {
-          po: tmp[1], order: tmp[0], color: tmp[2], date: myDate.getFullYear() + "-" + (myDate.getMonth() + 1 < 10? '0' + (myDate.getMonth() + 1) : myDate.getMonth() + 1 ) + "-" + (myDate.getDate()<10?'0'+(myDate.getDate()): (myDate.getDate())),
-          size1: tmp[4], size2: tmp[5], size3: tmp[6], size4: tmp[7], size5: tmp[8], size6: tmp[9], size7: tmp[10], size8: tmp[11], size9: tmp[12], size10: tmp[13]
+          po: tmp[1], order: tmp[0], color: tmp[2], 
+          size1: tmp[3], size2: tmp[4], size3: tmp[5], size4: tmp[6], size5: tmp[7], size6: tmp[8], size7: tmp[9], size8: tmp[10], size9: tmp[11], size10: tmp[12]
         };
 
         Finish.add(data, function(err, result){
